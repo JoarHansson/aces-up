c8d9dca1314ac6da7c81dc62db0a37cf8491a2a1
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2023-10-19 14:06:00 +0200

    fix last unicode character of each card and improve comments

--- index.php
+++ index.php
@@ -10,30 +10,55 @@ $suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 $values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 $deck = [];
 
-// construct deck of cards, add unicodes to each card and shuffle the deck:
+// The output of these nested foreach loops is the a populated array $deck. 
+// It contains suit and value as well as the unicode for each card.
 foreach ($suits as $suit) {
-
   foreach ($values as $key => $value) {
+
+    $lastUnicodeChar; // (1,2,3,4,5,6,7,8,9,a,b,d,e)
+
+    switch (base_convert(strval(array_keys($values)[$key] + 1), 10, 16)) {
+      case "a":
+        $lastUnicodeChar = "a";
+        break;
+      case "b":
+        $lastUnicodeChar = "b";
+        break;
+      case "c":
+        $lastUnicodeChar = "d";
+        break;
+      case "d":
+        $lastUnicodeChar = "e";
+        break;
+      default:
+        $lastUnicodeChar = base_convert(strval(array_keys($values)[$key] + 1), 10, 16);
+    }
+
     $deck[] = [
       "suit" => $suit,
       "value" => $value,
       "unicode" =>
-      "U+1F0" . chr(65 + array_search($suit, $suits)) .
-        base_convert(strval(array_keys($values)[$key] + 1), 10, 16)
+      "U+1F0" . chr(65 + array_search($suit, $suits)) . $lastUnicodeChar,
     ];
 
     /*
     Value for key ($deck[x]["unicode"] explained:
-    chr(65 + array_search($suit, $suits)) == A,B,C or D.
-    array_keys($values)[$key] == a number, 0 to 12.
-      1 is then added because the unicode indexes start at 1, not 0.
-    base_convert($int, 10, 16) converts to hexadecimal (again, because of unicode indexes)
+
+      * "U+1F0" - all cards start with this substring.
+
+      * chr(65 + array_search($suit, $suits)) == A,B,C or D (depending on suit)
+
+      * $lastUnicodeChar:
+        - array_keys($values)[$key] == a number, 0 to 12.
+        - 1 is then added because the unicode indexes start at 1, not 0.
+        - base_convert($int, 10, 16) converts number to hexadecimal (because of unicode indexes)
 
     See this chart for more clarity:
     https://en.wikipedia.org/wiki/Playing_Cards_(Unicode_block)
     */
   }
 }
+
 shuffle($deck);
 
 dealCards($deck, $handOfCards);
